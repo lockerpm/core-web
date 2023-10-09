fb5d86e6c166f6c893e30a9d7a7b4c955081eff7
quyetnv <quyetnv@cystack.net>
2023-10-09 11:58:10 +0700

    feat: update core

--- src/core-js/src/importers/importer.ts
+++
@@ -1,6 +0,0 @@
-import { ImportResult } from '../../src/models/domain/importResult'
-
-export interface Importer {
-  organizationId: string;
-  parse(data: string): Promise<ImportResult>;
-}
--- src/core-js/src/importers/onepasswordImporters/cipherImportContext.ts
+++
@@ -1,8 +0,0 @@
-import { CipherView } from '../../../src/models/view'
-
-export class CipherImportContext {
-  lowerProperty: string
-  constructor(public importRecord: any, public property: string, public cipher: CipherView) {
-    this.lowerProperty = property.toLowerCase()
-  }
-}
