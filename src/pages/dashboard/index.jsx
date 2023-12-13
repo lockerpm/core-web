caaedf0cc3554ecb4f31fbe9cea5192b8c84e8de
quyetnv <quyetnv@cystack.net>
2023-12-13 18:18:27 +0700

    feat: update file

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -2,7 +2,7 @@ import Enterprises from './enterprises'
 import EmailSettings from './email-settings'
 import SSOConfiguration from './sso-configuration'
 
-import EnterpriseDashboard from './enterprise/index'
+import EnterpriseDashboard from './enterprise/dashboard'
 import EnterpriseMembers from './enterprise/members'
 import EnterpriseGroups from './enterprise/groups'
 import EnterprisePolicies from './enterprise/policies'
