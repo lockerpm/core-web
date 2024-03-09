edc17d54053d5650ffe4f298a931c97650943fb6
quyetnv <quyetnv@cystack.net>
2024-03-09 14:57:14 +0700

    fix: refactor code bugs

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -5,7 +5,7 @@ import { initReactI18next } from 'react-i18next'
 import en from '../locale/en.json'
 import vi from '../locale/vi.json'
 
-import common from '../utils/common'
+import storage from '../utils/common/storage'
 
 const resources = {
   en: {
@@ -19,7 +19,7 @@ const resources = {
 i18n.use(initReactI18next)
   .init({
     resources,
-    lng: common.getLanguage(),
+    lng: storage.getLanguage(),
     interpolation: {
       escapeValue: false,
     },
