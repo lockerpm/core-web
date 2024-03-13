5d025333d53e42b2b5c7ce3faeb0799d0dffadce
quyetnv <quyetnv@cystack.net>
2024-03-13 11:45:57 +0700

    fix: init global

--- src/config/global.js
+++ src/config/global.js
@@ -1,14 +1,25 @@
+import keys from '../config/keys'
+import constants from '../config/constants'
+import patterns from '../config/patterns'
+import rules from '../config/rules'
+import endpoint from '../config/endpoint'
+import urls from '../config/urls'
+import routers from '../config/routers'
+import menus from '../config/menus'
+import store from '../store'
+
+global.store = store
 export default {
+  endpoint,
+  constants,
+  routers,
+  menus,
+  patterns,
+  rules,
+  store,
+  keys,
+  urls,
   jsCore: null,
-  endpoint: {},
-  constants: {},
-  routers: {},
-  menus: {},
-  patterns: {},
-  rules: {},
-  store: {},
-  keys: {},
-  urls: {},
   location: {},
   confirm: () => {},
   notification: () => {},
--- src/utils/provider.js
+++ src/utils/provider.js
@@ -10,44 +10,17 @@ import vi from '../locale/vi.json'
 import en from '../locale/en.json'
 import global from '../config/global'
 
-import keys from '../config/keys'
-global.keys = keys
-
-import constants from '../config/constants'
-global.constants = constants
-
-import patterns from '../config/patterns'
-global.patterns = patterns
-
-import rules from '../config/rules'
-global.rules = rules
-
-import endpoint from '../config/endpoint'
-global.endpoint = endpoint
-
-import urls from '../config/urls'
-global.urls = urls
-
-import routers from '../config/routers'
-global.routers = routers
-
-import menus from '../config/menus'
-global.menus = menus
-
-import store from '../store'
-global.store = store
-
 const Locales = props => {
   const localeIntl = {
     locale: props.locale,
-    messages: props.locale === constants.LANGUAGE.EN ? en : vi,
+    messages: props.locale === global.constants.LANGUAGE.EN ? en : vi,
   }
   moment.locale(props.locale)
   
   return (
     <IntlProvider {...localeIntl}>
       <ConfigProvider
-        locale={props.locale === constants.LANGUAGE.EN ? en_US : vi_VN}
+        locale={props.locale === global.constants.LANGUAGE.EN ? en_US : vi_VN}
       >
         {props.children}
       </ConfigProvider>
