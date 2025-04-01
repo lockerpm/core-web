ae9661ed645f519d583e9f71854b49f606aea982
quyetnv <quyetnv@cystack.net>
2025-04-01 14:10:13 +0700

    fix: craco config

--- craco.config.js
+++ craco.config.js
@@ -29,19 +29,21 @@ module.exports = {
   webpack: {
     configure: (config) => {
       config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'process/browser': require.resolve("process/browser"),
+        process: require.resolve("process/browser"),
         crypto: require.resolve("crypto-browserify"),
         stream: require.resolve("stream-browserify"),
         buffer: require.resolve("buffer"),
-        process: require.resolve("process/browser"),
         vm: false,
       };
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+          process: "process/browser",
+        })
+      );
       return config;
     },
-    plugins: [
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-        process: "process/browser",
-      }),
-    ],
   },
 }
