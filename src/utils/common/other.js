0404e7a2eb6e6e6ba1ca67ec5d20d9538fb3953a
quyetnv <quyetnv@cystack.net>
2026-08-12 17:46:11 +0700

    fix: bugs

--- src/utils/common/other.js
+++ src/utils/common/other.js
@@ -57,7 +57,7 @@ const getColorByIndex = index => {
 }
 
 const openNewTab = (link) => {
-  if (patterns.LINK.test(link)) {
+  if (!patterns.LINK.test(link)) {
     global.pushError({
       message: <Trans i18nKey='validation.invalid' values={{ name: 'URL' }} />
     });
